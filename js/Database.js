7051053b4a585369dafeec774f54944224aafda9
ibrahim0891 <nijhum0891@gmail.com>
2025-08-25 13:09:22 +0600

    added transection history , improved code quality and added custom cursor

--- js/Database.js
+++ js/Database.js
@@ -13,50 +13,96 @@ let Bank = {
             pin,
             mobile,
             balance: Number(initialDeposit),
+            transectionHistory: savedUserInfo[id] == undefined ? [] : savedUserInfo[id].transectionHistory , 
             deposite: function (amount, transectionType = 'self', pin) {
                 switch (transectionType) {
                     case 'bank-transfer':
                         this.balance += amount
                         break;
                     case 'self' || undefined:
-                        pin == this.pin ? this.balance += amount : alert('wrong pin ')
+                        if (pin == this.pin) {
+                            this.balance += amount;
+                            let diposit = {
+                                name: `Add money to account`,
+                                time: new Date().toLocaleString(),
+                                amount
+                            }
+                            this.transectionHistory.unshift(diposit) //feeling lazy to fix spelling
+                        } else {
+                            alert('wrong pin ');
+                        }
                 }
             },
             withdraw: function (amount, transectionType = 'self', pin) {
                 switch (transectionType) {
                     case 'bank-transfer':
-                        this.balance - amount > -1 ?
-                            this.balance -= amount : alert('taka nai')
+                        if (this.balance - amount > -1) {
+                            this.balance -= amount;
+                        } else {
+                            alert('taka nai');
+                        }
                         break;
                     case 'self' || undefined:
-                        pin == this.pin ? this.balance - amount > -1 ?
-                            this.balance -= amount : alert('taka nai') : alert('wrong pin ')
+                        if (pin == this.pin) {
+                            if (this.balance - amount > -1) {
+                                this.balance -= amount;
+                                let withdraw = {
+                                    name: `Cash out`,
+                                    time: new Date().toLocaleString(),
+                                    amount
+                                }
+                                this.transectionHistory.unshift(withdraw) //feeling lazy to fix spelling
+                            } else {
+                                alert('taka nai');
+                            }
+                        } else {
+                            alert('wrong pin ');
+                        }
                 }
-            }
+            },
         }
     },
 
-    transferMoney: (from, to, amount, pin) => {
+    transferMoney: (from, to, amount, pin) => { 
         let sender = BankDatabase[from];
         let receiver = BankDatabase[to]
 
         let loggedInUserId = getSavedData('user').id
+
+        let senderRecord = {
+            name: 'Sent money to ' + receiver.name,
+            time: new Date().toLocaleString(),
+            amount
+        }
+        let receiverRecord = {
+            name: 'Received Money from ' + sender.name,
+            time: new Date().toLocaleString(),
+            amount,
+        }
+
+
+        sender.transectionHistory.unshift(senderRecord)
+        receiver.transectionHistory.unshift(receiverRecord)
+
+        
         if (receiver.id == loggedInUserId) {
             alert('batpari to valoi jano!')
         }
-        console.log(loggedInUserId);
+        
         if (sender.pin == pin) {
             sender.withdraw(amount, 'bank-transfer')
             receiver.deposite(amount, 'bank-transfer')
         } else {
             alert('Invalid pin')
         }
+ 
+        saveData('database', BankDatabase)
     },
 
     login: (mobile, pin) => {
         let validUser;
         for (const id in BankDatabase) {
-            let account = BankDatabase[id]
+            let account = savedUserInfo[id]
             if (account.mobile == mobile && account.pin == pin) {
                 validUser = { ...account, pin: null }
                 break;
@@ -79,13 +125,17 @@ let Bank = {
 
 
 if (Object.keys(savedUserInfo).length != 0) {
+    //jehetu amader kichu info saved ache ,sehetu amra segula use kore runtime e bankaccout create korte pari 
+    //jekhane amader withdraw() deposite() method gula access kora jabe
+    //eta kora lagteche because localstorage e only string rakha jay, function rakha jay na . 
     for (const key in savedUserInfo) {
         let user = savedUserInfo[key]
-        Bank.createAccount(user.id, user.name, user.mobile, user.pin, user.balance)
+        Bank.createAccount(user.id, user.name, user.mobile, user.pin, user.balance , user.transectionHistory)
     }
 } else {
-    Bank.createAccount('101', 'Alice', '12345678', '1234', 1000);
-    Bank.createAccount('102', 'Bob', '87654321', '5678', 1000);
+    //localstrogae jehutu ekebare empty tai amra default information diye account khule dicchi
+    Bank.createAccount('101', 'Ibrahim', '12345678', '1234', 1000);
+    Bank.createAccount('102', 'Nabil', '87654321', '5678', 1000);
     saveData('database', BankDatabase)
 }
 
--- js/index.js
+++ js/index.js
@@ -1,39 +1,41 @@
 
 
 
-import {getSavedData , returnElementById, saveData} from './utils.js'
+import { getSavedData, returnElementById, saveData } from './utils.js'
 import { Bank, BankDatabase } from './Database.js';
 
 let user = getSavedData('user')
 
-if(!user){
+if (!user) {
     window.location.href = './login.html'
 }
 
 let displayBalance = returnElementById('current-balance')
 let showUserInfo = returnElementById('show-user-info')
 
-let userId = user.id 
-let currentBalance = BankDatabase[userId].balance 
+let userId = user.id
+let currentBalance = BankDatabase[userId].balance
+
 
 displayBalance.textContent = currentBalance
 showUserInfo.textContent = `${user.name} - Id: ${user.id}`
 
 let COUPON_NUMBER = '2025'
-let couponUsed = false; 
- 
+let couponUsed = false;
+
+
 
 let addMoneyButton = returnElementById('add-money-button')
 addMoneyButton.addEventListener('click', (e) => {
     e.preventDefault()
-    let amountToAdd = Number(returnElementById('amount-to-add').value) 
+    let amountToAdd = Number(returnElementById('amount-to-add').value)
     let addMoneyPin = returnElementById('add-money-pin').value
 
-    let user = BankDatabase[userId] 
-    user.deposite(amountToAdd  , 'self' , addMoneyPin )
+    let user = BankDatabase[userId]
+    user.deposite(amountToAdd, 'self', addMoneyPin)
     displayBalance.innerText = user['balance']
-
-    saveData('database',BankDatabase)
+    saveData('database', BankDatabase)
+    loadTransection()
 })
 
 
@@ -43,26 +45,51 @@ cashOutButton.addEventListener('click', (e) => {
     let amountToOut = Number(returnElementById('cash-out-amount').value)
     let cashOutPin = returnElementById('cash-out-pin').value
     let user = BankDatabase[userId]
-    user.withdraw(amountToOut , 'self' , cashOutPin)
+    user.withdraw(amountToOut, 'self', cashOutPin)
     displayBalance.innerText = user['balance']
-
-    saveData('database',BankDatabase)
+    saveData('database', BankDatabase)
+    loadTransection()
 })
 
 
 //transfer 
 let transferButton = returnElementById('transfer-button')
+let displayTransection = returnElementById('load-transection')
+
+let loadTransection = () => {
+     displayTransection.innerHTML = ''
+    BankDatabase[userId].transectionHistory.forEach(element => {
+        return displayTransection.innerHTML += `
+          <div class="flex flex-col gap-4">
+              <div class="flex items-center justify-start bg-white rounded-lg p-4 gap-4">
+                  <img class="w-12 bg-gray-100 p-2 rounded-md" src="./assets/money1.png" alt="">
+                  <div>
+                      <h3 class='font-bold text-lg'> ${element.name} </h3>
+                      <p>
+                          <span>
+                              ${element.amount}$ 
+                          </span> at 
+                          <span> ${element.time} </span>
+                      </p>
+                  </div>
+              </div>
+          </div>
+          `
+    });
+     
+}
+
+loadTransection()
 
 transferButton.addEventListener('click', (e) => {
     e.preventDefault();
     let receiverId = returnElementById('receiver-id').value
     let amountToTransfer = Number(returnElementById('transfer-amount').value)
     let transferPin = returnElementById('transfer-pin').value
-    Bank.transferMoney(user.id , receiverId , amountToTransfer , transferPin)
-    
+    Bank.transferMoney(user.id, receiverId, amountToTransfer, transferPin)
+
     displayBalance.innerText = BankDatabase[userId].balance
-     
-    saveData('database',BankDatabase)
+    loadTransection()
 })
 
 
@@ -72,10 +99,19 @@ let couponButton = returnElementById('coupon-button')
 couponButton.addEventListener('click', (e) => {
     e.preventDefault();
     let couponNumber = returnElementById('coupon-number').value
-    let couponGift  = 100
+    let couponGift = 100
     let user = BankDatabase[userId]
     couponNumber == COUPON_NUMBER && couponUsed == false ? user.deposite(couponGift, 'bank-transfer') : alert('Ek bonus koybar nibi vai! Eto lov ken?')
-    couponUsed = true  
+    let bonusTransection = {
+        name : 'Got coupon bonus' , 
+        time : new Date().toLocaleString() , 
+        amount : couponGift
+    }
+    user.transectionHistory.unshift(bonusTransection)
+    loadTransection()
+    couponUsed = true
     displayBalance.innerText = user['balance']
-    saveData('database',BankDatabase)
+    saveData('database', BankDatabase)
 })
+
+
